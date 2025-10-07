3747e05dd5d937e9d764b37e4613bb5b0260b333
Tafari <Tafarigeorge5079@gmail.com>
2025-10-07 17:42:09 -0400

    Tested route data response

--- workingDirectory/index.js
+++ workingDirectory/index.js
@@ -6,5 +6,11 @@ app.listen(PORT, () => {
 })
 
 app.get('/data', (req, res) => {
+    //.status() must be connected to
+    //receive data from api.
+    res.status(200).send({
+        data1: '21.376 ZEC',
+        data2: '12.566 XMR'
+    })
     
 })
